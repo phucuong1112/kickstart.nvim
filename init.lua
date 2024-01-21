00699efb05f5a2472c7ca5a5f0c4de3734a6f4e9
cuongnp <phucuong1112@gmail.com>
2024-01-21 21:55:43 +0700

    feat: add vim config

--- init.lua
+++ init.lua
@@ -659,4 +659,5 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- require('config.keymaps');
+require('config.options');
+require('config.keymaps');
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+
+  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+}
